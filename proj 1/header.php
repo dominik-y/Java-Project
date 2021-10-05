ff7b250613d8ff1b9643946b13579c97f3f39f4a
Pavel Spirovski Ehmer <71426632+pspirovski@users.noreply.github.com>
2021-10-05 09:34:57 +0200

    Add files via upload

---
+++ proj 1/header.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-fixed-top navbar-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">Events</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Registrations</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="manager.php">Manager</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin.php">Admin</a>
+            </li>
+        </ul>
+    </div>
+</nav>
